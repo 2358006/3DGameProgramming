d180ee60d3511b765d8f68c92df1ff118b14aee0
김민영 <2358006@mokwon.ac.kr>
2025-03-18 11:21:37 +0900

    Week2

--- main.cpp
+++ main.cpp
@@ -1,7 +1,11 @@
 #include <GL/glut.h>
 void draw(void) {
     glColor3f(1.0, 1.0, 1.0);
-    glutWireTeapot(0.5);
+    //glutWireTeapot(0.5);
+    glVertex3d(0.5, 0.5, 0.0);
+    glVertex3d(-0.5, 0.5, 0.0);
+    glVertex3d(-0.5, -0.5, 0.0);
+    glVertex3d(0.5, -0.5, 0.0);
     glFlush();
 }
 
@@ -10,7 +14,7 @@ int main(int argc, char** argv) {
     glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
     glutInitWindowSize(500, 500);
     glutInitWindowPosition(300, 300);
-    glutCreateWindow("12000000 인트아이");
+    glutCreateWindow("Test");
     glutDisplayFunc(draw);
     glutMainLoop();
     return 0;
